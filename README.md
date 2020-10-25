2ab40e4ce3739098ca2f181940fb908ad9bd16fd
Cory Parsnipson <cparsnipson@gmail.com>
2020-10-25 01:12:43 -0700

    Add arduino sketch

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # arduino-hid-gamepad-test
+
+This is a test program for a game controller made with an Arduino Pro Micro.
+
+## Background
+
+The program uses the NicoHood HID library which, by the way, can be found in the
+official Arduino library manager. The controller uses 10 generic push button switches
+from a random electronics starter kit, plus a nintendo switch replacement thumbstick module
+wired up to the breadboard using a 5 position 0.5mm pitch FPC (fine-pitch connector) from digikey
+and a 6 position 0.5mm FPC breakout board.
+
+## Parts Reference
+
+Link to Nintendo Switch Replacement Parts:
+https://www.amazon.com/gp/product/B08BL4LY84/ref=ppx\_yo\_dt\_b\_asin\_title\_o02\_s00?ie=UTF8&psc=1
+
+Link to FPC connector:
+https://www.digikey.com/short/znv9p3
+
+^^ I found out what this was from this helpful page:
+https://github.com/dekuNukem/Nintendo\_Switch\_Reverse\_Engineering/issues/59
+https://github.com/dekuNukem/Nintendo\_Switch\_Reverse\_Engineering
+
+Link to breakout board:
+https://www.amazon.com/gp/product/B0191ELDL0/ref=ppx\_yo\_dt\_b\_asin\_title\_o00\_s00?ie=UTF8&psc=1
+
+##  Nintendo Switch Thumbstick
+
+It is important to note that the Switch thumbstick is designed to work with 1.8V apparently (see
+the Nintendo Switch Reverse Engineering git repo above). Using the wrong voltage will result in
+the joystick being "lopsided". That is, one direction of an axis will have a bigger range than 
+the opposite direction (e.g. x minus direction has a delta of 200 while x plus direction has a delta
+of 10000).
+
+The raw range of the thumbstick appears to be:
+
+x axis -> min: 80, max: 315
+y axis -> min: 50, max: 303
+
+In the arduino program, I needed to add some leeway for the analog value mapping because the
+joystick output signal is noisy. About 5- 10 in both directions was good:
+
+x axis -> min: 70, max: 325
+y axis -> min: 45, max: 315
---
+++ arduino-hid-gamepad-test.ino
@@ -0,0 +1,119 @@
+#include <HID-Project.h>
+#include <HID-Settings.h>
+
+const int pinLed = 2;
+
+const int pinDpadLeft = 3;
+const int pinDpadRight = 4;
+const int pinDpadUp = 5;
+const int pinDpadDown = 6;
+
+const int pinX = 7;
+const int pinY = 8;
+const int pinA = 9;
+const int pinB = 10;
+
+const int pinL3 = 14;
+const int pinR3 = 15;
+
+const int pinMeta = 16;
+
+const int NUM_BUTTONS = 10;
+
+const int pinLXAxis = A1;
+const int pinLYAxis = A0;
+
+int* idx2Pin = new int[NUM_BUTTONS];
+uint32_t* pressed = new uint32_t(0);
+
+void setup() {
+  pinMode(pinLed, OUTPUT);
+  
+  pinMode(pinDpadLeft, INPUT_PULLUP);
+  pinMode(pinDpadRight, INPUT_PULLUP);
+  pinMode(pinDpadUp, INPUT_PULLUP);
+  pinMode(pinDpadDown, INPUT_PULLUP);
+
+  pinMode(pinX, INPUT_PULLUP);
+  pinMode(pinY, INPUT_PULLUP);
+  pinMode(pinA, INPUT_PULLUP);
+  pinMode(pinB, INPUT_PULLUP);
+
+  pinMode(pinL3, INPUT);
+  digitalWrite(pinL3, HIGH);
+  
+  pinMode(pinMeta, INPUT_PULLUP);
+
+  idx2Pin[0] = pinDpadLeft;
+  idx2Pin[1] = pinDpadRight;
+  idx2Pin[2] = pinDpadUp;
+  idx2Pin[3] = pinDpadDown;
+
+  idx2Pin[4] = pinX;
+  idx2Pin[5] = pinY;
+  idx2Pin[6] = pinA;
+  idx2Pin[7] = pinB;
+
+  idx2Pin[8] = pinL3;
+
+  idx2Pin[9] = pinMeta;
+
+  digitalWrite(pinLXAxis, LOW);
+  digitalWrite(pinLYAxis, LOW);
+
+  Gamepad.begin();
+}
+
+bool isPullup(int pinId) {
+  if (pinId == pinL3 || pinId == pinR3) {
+    return false;
+  }
+  return true;
+}
+
+bool getPressed(int idx) {
+  if (idx == 0 || idx > 32) {
+    return 0;
+  }
+  return (*pressed >> (idx - 1)) & 0x1;
+}
+
+void setPressed(int idx, bool val) {
+  if (idx == 0 || idx > 32) {
+    return;
+  }
+
+  if (val) {
+    *pressed |= 0x1 << (idx - 1);
+  } else {
+    *pressed &= ~(0x1 << (idx - 1));
+  }
+}
+
+void loop() {
+  for (int idx = 0; idx < NUM_BUTTONS; ++idx) {
+    int button = idx + 1;
+    int pinIsHigh = digitalRead(idx2Pin[idx]);
+    if (!isPullup(idx2Pin[idx])) {
+      pinIsHigh = !pinIsHigh;
+    }
+
+    if (pinIsHigh) {
+      if (!getPressed(button)) {
+        setPressed(button, true);
+        Gamepad.press(button);
+      }
+    } else {
+      setPressed(button, false);
+      Gamepad.release(button);
+    }
+
+    // analog axes
+    Gamepad.xAxis(map(analogRead(pinLXAxis), 70, 325, -32767, 32767));
+    Gamepad.yAxis(map(analogRead(pinLYAxis), 45, 315, 32767, -32767)); // flip Y axis because I have it oriented upside-down
+
+    Gamepad.write();
+  }
+
+  digitalWrite(pinLed, *pressed == 0 ? LOW : HIGH);
+}
